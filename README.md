8967a4961e602a944da83e7769e846bd9237939b
jbees918 <5590@holbertonstudents.com>
2023-08-13 19:35:43 -0500

    stacks init

---
+++ README.md
@@ -0,0 +1,1 @@
+This is my readme for Monty at Holberton Tulsa
---
+++ main.c
@@ -0,0 +1,22 @@
+#include "monty.h"
+
+global_t global = {NULL, NULL};
+
+/**
+ * main - monty starter
+ * @argc: argument count
+ * @argv: argument value
+ * Return: 0 success
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc == 2)
+		monty_read(argv[1]);
+	else
+	{
+		dprintf(STDERR_FILENO, "USAGE: monty file\n");
+		exit(EXIT_FAILURE);
+	}
+	return (0);
+}
